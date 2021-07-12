6afc3cf17d045c6aa136df747d9986e102fc9593
m.dolgikh <mdolgikh@wsmgroup.com>
2021-07-12 18:01:48 +0300

    feat: add test video

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from "react"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import AdminUi from 'modules/Admin/ui/Form'
+import VideoUi from "modules/content/ui/Video"
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">Admin</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
@@ -30,16 +16,11 @@ const App = () => {
             <Users />
           </Route>
           <Route path="/">
-            <Home />
+            <VideoUi />
           </Route>
         </Switch>
-      </div>
     </Router>
   )
-}
-
-function Home() {
-  return <h2>Home</h2>
 }
 
 function Users() {
