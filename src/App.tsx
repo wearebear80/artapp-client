0f26f93f651dce0aab9fd27015c81f901c0b3faf
m.dolgikh <mdolgikh@wsmgroup.com>
2021-07-23 19:57:12 +0300

    fix: unresolve imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import CheckUI from "modules/check"
-import AOS from "aos"
-import "aos/dist/aos.css" // You can also use <link> for styles
-
-AOS.init()
 
 const App = () => {
   return (
